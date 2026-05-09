8a247dc3aa0ad221c7460e7007cb34fdf5a82dee
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-05-09 08:40:18 +0800

    Update Context cpp

--- src/context.cpp
+++ src/context.cpp
@@ -212,18 +212,6 @@ LsContext::LsContext(const Hooks::DeviceInfo& info, VkSwapchainKHR swapchain,
 
 #endif
 
-    // prepare render passes (desktop path)
-    this->cmdPool = Mini::CommandPool(info.device, info.queue.first);
-
-    for (size_t i = 0; i < 8; i++) {
-        auto& pass = this->passInfos.at(i);
-
-        pass.renderSemaphores.resize(conf.multiplier - 1);
-        pass.acquireSemaphores.resize(conf.multiplier - 1);
-        pass.postCopyBufs.resize(conf.multiplier - 1);
-        pass.postCopySemaphores.resize(conf.multiplier - 1);
-        pass.prevPostCopySemaphores.resize(conf.multiplier - 1);
-    }
 }
 
 VkResult LsContext::present(const Hooks::DeviceInfo& info, const void* pNext, VkQueue queue,
